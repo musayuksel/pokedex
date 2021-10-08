0a255f801e6e28e9d666f27905aae718ab526a59
musayuksel <musayuxel@gmail.com>
2021-10-08 18:02:38 +0100

    feat:add week-2 Exercise E PokedexList

--- src/components/CaughtPokemon.js
+++ src/components/CaughtPokemon.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 export default function CaughtPokemon(props) {
-  const [caught, setCaught] = useState(0);
-  const catchPokemon = () => setCaught((count) => count + 1);
-
+  // const [caught, setCaught] = useState(0);
+  // const catchPokemon = () => setCaught((count) => count + 1);
+  const [caught, setCaught] = useState([]);
+  const pokedexPool = ["Spearow", "Ditto", "Butterfree", "Pikachu", "Eevee"];
+  const catchPokemon = () => {
+    setCaught((arr) => {
+      const randomIndex = Math.floor(Math.random() * pokedexPool.length);
+      return arr.concat(pokedexPool[randomIndex]);
+    });
+  };
+  const pokedexList = caught.map((pokedex, i) => (
+    <li key={`pokedex${i}`}>{pokedex}</li>
+  ));
   return (
     <>
       <p>
-        Caught {caught} Pokemon on {props.date}
+        Caught {caught.length} Pokemon on {props.date}
       </p>
       <button onClick={catchPokemon}>Catch Pokemon</button>
+      <ul>{pokedexList}</ul>
     </>
   );
 }
