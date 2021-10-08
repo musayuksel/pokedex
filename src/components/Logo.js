f55e3acf45ba29ebc37fb714bd3f4ec033844eb5
musayuksel <musayuxel@gmail.com>
2021-10-08 14:36:11 +0100

    feat:add week-2 Exercise A

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,15 @@
 export default function Logo(prop) {
+  const logWhenClicked = () => {
+    console.log("YOU CLICKED TO THE LOGO...");
+  };
   return (
     <header>
       <h1>Welcome to the {prop.appName}</h1>
       <img
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt="pokemon"
+        onClick={logWhenClicked}
+        style={{ cursor: "pointer" }}
       />
     </header>
   );
