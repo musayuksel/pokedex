77dd1c0e06f9878c56aa4aa198ab7b16a602c609
musayuksel <musayuxel@gmail.com>
2021-10-15 10:58:15 +0100

    feat:Add extra Logo click event

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,23 @@
+import react from "react";
+
 export default function Logo({ appName, handleClick }) {
+  const useState = react.useState;
+  const [isShow, setIsShow] = useState(true);
+  function changeIsShow() {
+    setIsShow(!isShow);
+  }
   return (
     <header>
-      <h1>Welcome to the {appName}</h1>
-      <img
-        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
-        alt="pokemon"
-        onClick={handleClick}
-        style={{ cursor: "pointer" }}
-      />
+      <h1 onClick={handleClick}>Welcome to the {appName}</h1>
+
+      {isShow ? (
+        <img
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+          alt="pokemon"
+          onClick={changeIsShow}
+          style={{ cursor: "pointer" }}
+        />
+      ) : null}
     </header>
   );
 }
